90d0bc64d8d7c5d8efcff80d8c23914819dd20f0
dlink13 <denisemelyanenko@gmail.com>
2017-08-02 18:34:58 +0300

    c
    d

--- js/custom.js
+++ js/custom.js
@@ -1,111 +1,35 @@
+  t = $("html");
 
-$(document).ready(function() { 
-
-	$(".popup").magnificPopup();
-
-
-	//Animated scrolling		   
-	$('ul.mainmenu a').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-			var $target = $(this.hash);
-			$target = $target.length && $target || $('[id=' + this.hash.slice(1) +']');
-			if ($target.length) {
-				$('ul.mainmenu li').removeClass('active');
-				$(this).parent('li').addClass('active');
-				var targetOffset = $target.offset().top-200;
-				$('html,body').animate({scrollTop: targetOffset}, 1000);
-				return false;
-			}
-		}
-	});
-
-	// Scroll Down Link
-	$('.scrolldown').click(function() {
-		var targetOffset = $('div.blockquote').offset().top-100;
-		$('html,body').animate({scrollTop: targetOffset}, 1000);
-	});
-
-	// Menu Scroll Hide
-	var nav = $('.menubar');
-	var scroll = $('.menubar').attr('data-scroll');
-	$(function(){
-		$('.menubar').data('size','big');
-		if (scroll == 'false') {
-			nav.css({
-				marginTop:'0px'
-			});
-		};
-	});
-	$(window).scroll(function(){
-		if ($('body').scrollTop() > 0 && scroll == 'true') {
-			if (nav.data('size') == 'big') {
-				nav.data('size','small').stop().animate({
-					marginTop:'0px'
-				}, 300);
-			}
-		} else {
-			if (nav.data('size') == 'small' && scroll == 'true') {
-				nav.data('size','big').stop().animate({
-					marginTop:'-100px'
-				}, 300);
-			}
-			
-		}
-	});
-	// Text Rotator
-	$('.rotate').each(function(){
-		var el = $(this);
-		var text = $(this).html().split(",");
-		el.html(text[0]);
-		setInterval(function() {
-			el.animate({
-              textShadowBlur:20,
-              opacity: 0
-            }, 500 , function() {
-              index = $.inArray(el.html(), text)
-              if((index + 1) == text.length) index = -1
-              el.text(text[index + 1]).animate({
-                textShadowBlur:0,
-                opacity: 1
-              }, 500 );
-            });
-		}, 2000);
-	});
-
-$('.calc select').change(function(){
-          $edition = $('select#site_edition').val();
-          $design = $('select#site_design').val();
-          $code = $('select#site_code').val();
-          $site_cmsss = $('select#site_cms').val();
-         
-          $site_format = $('select#site_edition option:selected').attr('data-color');
-          $price = +$edition + (+$design + +$code + +$site_cmsss) ;
-          console.log($price);
-          $('span#final_price').text($price);
-          if ($site_format == 1) {
-            $('.card-images img').hide();   
-            $('#card_1_0').show();      
-          }
-          if ($site_format == 2) {
-            $('.card-images img').hide();   
-            $('#card_1_1').show();      
-          }
-          if ($site_format == 3) {
-            $('.card-images img').hide();   
-            $('#card_4_0').show();      
-          }
-          if ($site_format == 4) {
-            $('.card-images img').hide();   
-            $('#card_4_1').show();      
-          }
-          if ($site_format == 5) {
-            $('.card-images img').hide();   
-            $('#card_4_4').show();      
-          }
-                if ($site_format == 6) {
-                    $('.card-images img').hide();   
-                    $('#card_4_4').show();          
-                }
-        })
-
-});
+            var c = $("#menu").mmenu({
+              extensions: {
+                all: ["theme-black", "fx-menu-slide", "pagedim-black"],
+                "(min-width: 1430px)": ["widescreen"]
+              },
+              offCanvas: {
+                position: 'right'
+              },
+             /* указывается тема, анимация, затемнение основной части при открытии меню, 
+            также при ширине экрана больше 1430px меню не прячеться
+            */
+            navbar: {
+              title: "Здесь Будет Логотип"
+             }, //меняем заголовок. Можно использовать теги. Подробнее об аддоне navbar можно найти тут http://mmenu.frebsite.nl/documentation/addons/navbars.html
+             pageScroll : {
+             scroll : true, // прокрутка к якорю
+             update : true, //прокручивать, даже если пункт обозначен как активный
+             scrollOffset : 300 // отступ от якоря, по умолчанию 0. У меня почему-то не заработал :(
+            }
+             //добавляем или удаляем класс is-active гамбургеру при открытии или закрытии меню соответственно. Таймаут в 100мс оставлен чтобы анимация проходила после отрктия или закрытия и всё было плавно и без лагов.
+            }).data("mmenu"),
+            d = $("#hamburger").on("click", function(e) {
+              e.preventDefault(), t.hasClass("mm-opened") ? c.close() : c.open()
+            }).children(".hamburger");
+            c.bind("close:finish", function() {
+              setTimeout(function() {
+                d.removeClass("is-active")
+              }, 50)
+            }), c.bind("open:finish", function() {
+              setTimeout(function() {
+                d.addClass("is-active")
+              }, 50)
+            })
