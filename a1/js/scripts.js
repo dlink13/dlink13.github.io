787618c4444b439d6cac276bfabd9acea7288f4f
dlink13 <denisemelyanenko@gmail.com>
2017-10-14 19:41:07 +0300

    s

--- a1/js/scripts.js
+++ a1/js/scripts.js
@@ -245,3 +245,11 @@ function ipad_autoPlay() {
 	ipad_play(next);
 }
 
+function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+};
