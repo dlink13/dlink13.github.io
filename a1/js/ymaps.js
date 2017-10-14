5c461f6aa31e8cb1b3271937b45443edb10a0668
dlink13 <denisemelyanenko@gmail.com>
2017-10-14 10:55:58 +0300

    New

---
+++ a1/js/ymaps.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function(){
+    ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [55.766025, 37.58729],
+            zoom: 15
+        }),
+        myPlacemark = new ymaps.Placemark([55.766025, 37.58729], {
+            // Чтобы балун и хинт открывались на метке, необходимо задать ей определенные свойства.
+            balloonContentBody: "Садово-Кудринская ул., 15, стр. 5, Москва 228375"
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: prefix+'a1/img/c17_map_marker.png',
+            // Размеры метки.
+            iconImageSize: [40, 46],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-22, -48]
+        });
+
+    myMap.geoObjects.add(myPlacemark);
+
+    myMap.controls
+        // Кнопка изменения масштаба.
+        .add('zoomControl', { left: 5, top: 5 })
+        // Список типов карты
+        .add('typeSelector')
+        // Стандартный набор кнопок
+        .add('mapTools', { left: 35, top: 5 });
+});
+});
