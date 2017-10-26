88fcdbfa6dfdf45d03ac1f3f4483899aa7a48b0c
dlink13 <denisemelyanenko@gmail.com>
2017-10-26 20:54:43 +0300

    +

---
+++ js/custom.js
@@ -0,0 +1,50 @@
+
+var show;
+function hidetxt(type){
+param=document.getElementById(type);
+if(param.style.display == "none") {
+if(show) show.style.display = "none";
+param.style.display = "block";
+show = param;
+}else param.style.display = "none"
+};
+
+ var lastOpen;
+ function collapsElement(id) {
+ if ( document.getElementById(id).style.display != "none" ) {
+ document.getElementById(id).style.display = 'none';
+ }
+ else {
+ if(lastOpen !== undefined) {
+ lastOpen.style.display = 'none';
+ }
+ lastOpen = document.getElementById(id);
+ document.getElementById(id).style.display = '';
+ }
+ };
+
+
+function openbox(id){
+    display = document.getElementById(id).style.display;
+
+    if(display=='none'){
+       document.getElementById(id).style.display='block';
+    }else{
+       document.getElementById(id).style.display='none';
+    }
+};
+            function showHide(element_id) {
+                //Если элемент с id-шником element_id существует
+                if (document.getElementById(element_id)) { 
+                    //Записываем ссылку на элемент в переменную obj
+                    var obj = document.getElementById(element_id); 
+                    //Если css-свойство display не block, то: 
+                    if (obj.style.display != "block") { 
+                        obj.style.display = "block"; //Показываем элемент
+                    }
+                    else obj.style.display = "none"; //Скрываем элемент
+                }
+                //Если элемент с id-шником element_id не найден, то выводим сообщение
+                else alert("Элемент с id: " + element_id + " не найден!"); 
+            }; 
+
