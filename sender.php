7ba280eac5a6d7f8c5f45eb94c356cdad0549d33
dlink13 <denisemelyanenko@gmail.com>
2017-12-06 22:37:33 +0200

    +

--- sender.php
+++ sender.php
@@ -6,7 +6,6 @@ if ($_SERVER['HTTP_X_REQUESTED_WITH'] != 'XMLHttpRequest') {return;}
 //фильтруем запрос данных с формы
 
 $name = filter_input(INPUT_POST, 'name' );
-$email = filter_input(INPUT_POST, 'email' );
 $phone = filter_input(INPUT_POST, 'phone' );
 $ques = filter_input(INPUT_POST, 'ques' );
 $formname = filter_input(INPUT_POST, 'formname' );
@@ -22,7 +21,6 @@ $subject = "Заявка с сайта ";
 // Сообщение для более менее приглядного вида в HTML
 $message =   "Имя: " . $name . "\n";
 $message .=   "Телефон: " . $phone . "\n";
-$message .=   "Почта: " . $email . "\n";
 $message .=   "Вопрос: " . $ques . "\n";
 $message .=   "Имя формы: " . $formname . "\n";
 
