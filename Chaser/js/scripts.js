0ef6adcd04d237a2dc19c25c5998a9a4ddb8eb0a
dlink13 <denisemelyanenko@gmail.com>
2018-07-25 20:19:17 +0300

    ++

--- Chaser/js/scripts.js
+++ Chaser/js/scripts.js
@@ -1,4 +1,18 @@
+$(function(){
+  $("#example-one").on("click","a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
 
+    //забираем идентификатор бока с атрибута href
+    var id  = $(this).attr('href'),
+
+    //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
+    
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({scrollTop: top}, 1500);
+  });
+});
 (function ($) {   
     var is_empty = function (value) {
         return !value.trim();
