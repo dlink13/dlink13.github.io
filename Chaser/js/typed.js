50b516984dc2d9bb38b74edebd47a2cd1c4d5017
dlink13 <denisemelyanenko@gmail.com>
2018-08-01 19:42:42 +0300

    +++

--- Chaser/js/typed.js
+++ Chaser/js/typed.js
@@ -798,7 +798,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	   * @property {array} strings strings to be typed
 	   * @property {string} stringsElement ID of element containing string children
 	   */
-	  strings: [ ' Chaser', ' Chaser Team!', ],
+	  strings: [ ' CHASER', '  IT RECRUITMENT TEAM ! ', ],
 	  stringsElement: null,
 	
 	  /**
